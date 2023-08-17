69e81db745976072ca66f56a0235830ca5ee9722
LahariLanke <lahari.marolix@gmail.com>
2023-08-17 10:17:13 +0530

    CRUD

---
+++ djangocrud/app1/views.py
@@ -0,0 +1,48 @@
+from multiprocessing import context
+from django.shortcuts import render
+from .forms import EmployeeForm
+from .models import Employee
+from django.shortcuts import redirect
+# Create your views here.
+
+def Home(request):
+    form=EmployeeForm()
+    if request.method=='POST':
+        form=EmployeeForm(request.POST)
+        form.save()
+        form=EmployeeForm()
+
+    data=Employee.objects.all()
+            
+        
+    context={
+        'form':form,
+        'data':data,
+    }
+    return render(request,'app1/index.html',context)
+
+# Delete view
+def Delete_record(request,id):
+    a=Employee.objects.get(pk=id)
+    a.delete()
+    return redirect('/')
+ 
+# Update View
+def Update_Record(request,id):
+    if request.method=='POST':
+        
+        data=Employee.objects.get(pk=id)
+        form=EmployeeForm(request.POST,instance=data)
+         
+        if form.is_valid():
+            form.save()
+    else:
+        data=Employee.objects.get(pk=id)
+        form=EmployeeForm(instance=data)
+        
+    context={
+        'form':form,
+    }
+    return render(request,'app1/update.html',context)
+     
+
